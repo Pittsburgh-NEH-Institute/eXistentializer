84f96e8755a6183dd759668b9b48ba39558bda88
gabkeane <gabakeane@gmail.com>
2022-12-22 14:55:08 -0500

    initializer creates expath-pkg file with required package element

--- initializer.sh
+++ initializer.sh
@@ -2,10 +2,16 @@
 
 source properties.txt
 
-cat > expath-pkg.xml << EOL
-<package name="$name">
-this is a package element
+cat > expath-pkg.xml << XPATH_PKG_END
+<?xml version="1.0" encoding="UTF-8"?>
+<package
+    xmlns="http://expath.org/ns/pkg"
+    name="$project_URI/app/$name"
+    abbrev="$name"
+    version="$version"
+    spec="1.0">
+    <title>$description</title>
+    <dependency processor="http://exist-db.org" semver-min="$exist_version"/>
 </package>
-EOL
+XPATH_PKG_END
 
-echo "Hi mom!"
